45e433596e38c54b6a9dfcf34dc826a474f32984
deepanshugoyal-cse <deepanshugoyal.bt24cse@pec.edu.in>
2026-09-13 11:19:43 +0530

    Time: 11 ms (31.39%) | Memory: 31 MB (38.48%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
+        set<int> seen;
         int n = digits.size();
-        bool vis[1000]{};
-        int ans = 0;
-
-        for (int i = 0; i < n; ++i) {
-            if (digits[i] == 0) {
-                continue;
-            }
-            for (int j = 0; j < n; ++j) {
-                if (j == i) {
-                    continue;
-                }
-                for (int k = 0; k < n; ++k) {
-                    if (k == i || k == j || digits[k] % 2 != 0) {
-                        continue;
-                    }
-                    int x = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    if (!vis[x]) {
-                        vis[x] = true;
-                        ++ans;
-                    }
-                }
-            }
-        }
-
-        return ans;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    if (i != j && j != k && i != k && digits[i] && digits[k] % 2 == 0)
+                        seen.insert(digits[i]*100 + digits[j]*10 + digits[k]);
+        return seen.size();
     }
 };
